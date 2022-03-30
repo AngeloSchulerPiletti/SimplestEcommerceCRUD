12d5119b450715486de9e713709a2979c16fab4f
SchPil <angelopiletti@gmail.com>
2022-03-30 12:03:02 -0300

    Purchase DTOs done

---
+++ SimplestEcommerceCRUD.Domain/Objects/DTO/PurchaseItemDto.cs
@@ -0,0 +1,18 @@
+﻿using SimplestEcommerceCRUD.Domain.Entities;
+
+namespace SimplestEcommerceCRUD.Domain.Objects.DTO
+{
+    public class PurchaseItemDto
+    {
+        public PurchaseItemDto(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+            Total = (int)(product.Price * quantity);
+        }
+
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public int Total { get; set; }
+    }
+}
---
+++ SimplestEcommerceCRUD.Domain/Objects/DTO/PurchaseOrderDto.cs
@@ -0,0 +1,19 @@
+﻿namespace SimplestEcommerceCRUD.Domain.Objects.DTO
+{
+    public class PurchaseOrderDto
+    {
+        public PurchaseOrderDto(List<PurchaseItemDto> purchaseItems)
+        {
+            PurchaseItems = purchaseItems;
+            var total = 0;
+            foreach (PurchaseItemDto purchaseItem in purchaseItems)
+            {
+                total += purchaseItem.Total;
+            }
+            Total = total;
+        }
+
+        public List<PurchaseItemDto> PurchaseItems { get; set; }
+        public int Total { get; set; }
+    }
+}
